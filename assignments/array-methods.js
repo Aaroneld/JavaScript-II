3c35a6137f15b3e62da5a80e60deff9e2cc0ac13
Aaron Merrifield <merrifield48@gmail.com>
2019-08-11 16:57:39 -0400

    small change

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,8 +57,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
 runners.forEach(function (element){
-    let name = element.first_name + " " + element.last_name;
-    fullName.push(name);
+    fullName.push(element.first_name + " " + element.last_name);
   })
 console.log(fullName);
  
